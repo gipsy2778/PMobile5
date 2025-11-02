5b7170075452dc4398d16d0b15ed71a383427655
gipsy2778 <rafly9694@gmail.com>
2025-11-02 20:21:39 +0700

    2nd commit

--- lib/main.dart
+++ lib/main.dart
@@ -26,25 +26,25 @@ class HomePage extends StatelessWidget {
     final List<String> books = [
       'My Kisah',
       '10 Dosa Besar Soeharto',
-      'Lulus SMA Mending Kuliah dulu atau Langsung Meninggal?',
-      'Cara Mengubah Dosa jadi Saldo Dana',
-      'Kerja TOLOL!!!',
+      'Babi Ngesot: Datang Tak Diundang, Pulang Tak Berkutang',
+      'Cado-Cado: Catatan Dodol Calon Dokter',
+      'Nanti Kita Cerita Tentang Hari Ini',
     ];
 
     final List<String> descriptions = [
-      'My kisah menceritakan kisah manis antara Gwehj, Mahasiswa USUR yang berwajah garang tapi berhati lembut, dan My, siswi SMA Sumizome yang anggun dan sopan. Pertemuan mereka secara kebetulan di toko kue tempat Gwehj bekerja menjadi awal dari hubungan yang hangat namun penuh rintangan. Meski sekolah mereka memiliki persaingan lama dan perbedaan status sosial yang tajam, keduanya perlahan menemukan kenyamanan dalam kehadiran satu sama lain. Dengan latar kehidupan sekolah yang penuh tawa, kesalahpahaman, dan momen-momen kecil yang berkesan, kisah ini menggambarkan cinta remaja yang tumbuh diam-diam namun tulus.',
-      'Mengulas perjalanan panjang kekuasaan Presiden Soeharto, dengan fokus pada kebijakan, tindakan, dan dampak yang menimbulkan kontroversi dalam sejarah Indonesia. Melalui pendekatan kritis dan analitis, penulis berusaha menyoroti sepuluh aspek utama yang dianggap sebagai kesalahan besar selama masa pemerintahannya, mulai dari korupsi, pelanggaran HAM, pembungkaman kebebasan pers, hingga praktik nepotisme dan penyalahgunaan kekuasaan. Disajikan dengan bahasa yang lugas namun berimbang, buku ini mengajak pembaca untuk memahami sisi gelap Orde Baru, bukan untuk menghakimi semata, tetapi sebagai refleksi agar kesalahan serupa tidak terulang dalam perjalanan bangsa di masa depan.',
-      'Lu Bingung apa lagi Gw',
-      'Lhoo!! Emang bisa? Ya kagak lah kocak.',
-      'Kuliah itu scam.',
+      'Meskipun letaknya berdekatan, SMA Negeri Chidori dan Akademi Swasta Kikyo khusus perempuan seolah berada di dua dunia yang berbeda. Akademi Kikyo dikenal memiliki reputasi yang sempurna dan mayoritas siswanya berasal dari keluarga kaya, sementara Chidori dipenuhi oleh murid-murid nakal yang dianggap bodoh. Seluruh siswa Kikyo menyimpan kebencian mendalam terhadap tetangga mereka dari Chidori. Rintarou Tsumugi, seorang siswa Chidori berusia enam belas tahun, sering dijauhi orang karena wajahnya yang terlihat menyeramkan. Namun, Rintarou tidak terlalu mempedulikannya, karena ia sudah merasa cukup dengan teman-teman dekatnya. Meski begitu, ada satu hal yang belum ia ceritakan kepada mereka: sesekali, ia membantu di toko kue milik keluarganya. Suatu hari saat bekerja, Rintarou melihat seorang pelanggan duduk di meja, namun gadis itu pergi sebelum ia sempat berbicara dengannya. Keesokan harinya, ia menerima permintaan maaf dari gadis tersebut, Kaoruko Waguri, yang menjelaskan bahwa ia bukan kabur karena penampilan Rintarou, dan justru percaya bahwa Rintarou adalah orang yang baik hati. Meski Rintarou belum terbiasa dengan kepribadian cerah Kaoruko, ia mulai menantikan persahabatan dengan gadis itu. Sayangnya, ada satu rintangan besar yang menghadang: Kaoruko adalah siswa Kikyo!',
+      '10 Dosa Besar Soeharto" merupakan sebuah karya yang mengupas kepemimpinan mantan Presiden Indonesia, Soeharto. Buku ini menyajikan analisis kritis terhadap periode panjang pemerintahannya, mengidentifikasi dan mengulas sepuluh isu utama yang dikaitkan dengan kebijakan, politik, dan dampaknya terhadap masyarakat.',
+      'Babi Ngesot: Datang Tak Diundang, Pulang Tak Berkutang adalah kumpulan cerita pendek pengalaman pribadi Raditya Dika, penulis Indonesia terbodoh saat ini. Buku ini berisi tujuh belas cerita aneh-tapi-nyata Raditya, termasuk kalang kabut digencet kakak kelas, dihantuin setan rambut poni, sampai perjuangan menyelamatkan keteknya yang sedang "sakit". Buku ini diterbitkan pada tahun 2008 dan berisi 240 halaman. Raditya Dika, yang juga dikenal sebagai penulis Kambingjantan: Catatan Harian Pelajar Bodoh dan Cinta Brontosaurus, kini bekerja sebagai direktur dan editor-in-chief Bukune Publishing House.',
+      'Dalam buku Cado-Cado : Catatan Dodol Calon Dokter, kamu akan menyimak 14 pengalaman lucu Riva sewaktu menjalani koass atau pendidikan lanjut mahasiswa kedokteran dulu. Dari mulai dijebloskan ke ruangan penuh orang sakit jiwa sampai gemeteran merawat pasien waria.',
+      'Nanti Kita Cerita Tentang Hari Ini adalah novel yang ditulis oleh Marchella FP, yang mengisahkan tentang seorang Wanita bernama Awan, yang berusia 27 tahun dan seorang ibu. Cerita ini menggali tema masa depan dan konflik dalam keluarga yang terpendam, yang dapat merusak keharmonisan keluarga. Buku ini sangat direkomendasikan bagi mereka yang merasa gelisah atau sedih, karena dapat memberikan kata-kata penyemangat dan penenang emosi. co Gramedia.com +3',
     ];
 
     final List<String> images = [
       'assets/images/my.png',
       'assets/images/top10.png',
-      'assets/images/baru_lulus.png',
-      'assets/images/saldo_dana.png',
-      'assets/images/kerja_tolol.png',
+      'assets/images/b2.png',
+      'assets/images/cado_cado.png',
+      'assets/images/cerita.png',
     ];
 
     return Scaffold(
